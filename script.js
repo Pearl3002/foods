66e9d1736c4c473d9493b3a95f44ee1e222b217d
Pearl3002 <126729082+Pearl3002@users.noreply.github.com>
2023-06-09 22:46:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ menu.addEventListener("click", function(){
 window.onscroll = () => {
     navbar.classList.remove("active");
 };
+
+//cta popup
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+
+function closepopup(){
+    popup.classList.remove("open-popup");
+}
+
+
+//send email with javascript
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "bookie.abayomi.2003@gmail.com",
+        Password : "0CD78A2D94E062F902995B80A424408EDBD4",
+        To : 'bookie.abayomi.2003@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New content form Enquiry",
+        Body : "Name" + document.getElementById("name").value 
+        + "<br> Email" + document.getElementById("email").value
+        + "<br> Phone no." + document.getElementById("phone").value
+        + "<br> Message" + document.getElementById("message").value,
+    }).then(
+      message => alert("Message sent successfully")
+    );
+}
