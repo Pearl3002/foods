9564324938b386d3a0177076415059a6f90bc549
Pearl3002 <126729082+Pearl3002@users.noreply.github.com>
2023-06-11 17:44:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function closepopup(){
 
 
 //send email with javascript
+    >
+     const scriptURL = '<https://script.google.com/macros/s/AKfycbzvsWaKnM3yk2FzxkOI6H01je-q6JE0eosZrh0gcNgfb1o4f2Uj_8qndRhxNb-NjOCRLA/exec>'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML =""
+        }, 4000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+    
+
+
+
+
